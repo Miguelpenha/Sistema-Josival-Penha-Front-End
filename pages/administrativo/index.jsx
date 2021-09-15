1e471f29fa145eb43a18bcc1ea2f99385c7c8bb8
Miguel Penha <miguel.penha1@gmail.com>
2021-09-15 20:18:56 -0300

    Usando os ícones

--- pages/administrativo/index.jsx
+++ pages/administrativo/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import nookies from 'nookies'
-import { Container, NavOptions, LogoJPNome, Funções, Função, IconAlunos, Main } from '../../styles/pages/administrativo'
+import { Container, NavOptions, LogoJPNome, Funções, Função, IconAlunosSele, IconAcadêmico, IconDashBoard, IconMarketing, IconFinanceiro, IconColaboradores, Main } from '../../styles/pages/administrativo'
 import api from '../../api/api'
 
 export default function Administrativo() {
@@ -15,14 +15,29 @@ export default function Administrativo() {
           <LogoJPNome/>
           <Funções>
             <Função selected={true}>
-              <IconAlunos/>
+              <IconAlunosSele/>
               Alunos
             </Função>
-            <Função>Acadêmico</Função>
-            <Função>Dashboard</Função>
-            <Função>Marketing</Função>
-            <Função>Colaboradores</Função>
-            <Função>Financeiro</Função>
+            <Função>
+              <IconAcadêmico/>
+              Acadêmico  
+            </Função>
+            <Função>
+              <IconDashBoard/>
+              Dashboard
+            </Função>
+            <Função>
+              <IconMarketing/>
+              Marketing
+            </Função>
+            <Função>
+              <IconFinanceiro/>
+              Financeiro
+            </Função>
+            <Função>
+              <IconColaboradores/>
+              Colaboradores
+            </Função>
           </Funções>
         </NavOptions>
         <Main>
--- styles/pages/administrativo/index.js
+++ styles/pages/administrativo/index.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components'
 import LogoJPNomeSemStyle from '../../../components/LogoJPNome'
-import IconAlunosSVG from '../../../assets/icon-alunos.svg'
+import IconAlunosSVG from '../../../assets/icon-nav-admin-alunos.svg'
+import IconAlunosSeleSVG from '../../../assets/icon-nav-admin-alunos-sele.svg'
+import IconAcadêmicoSVG from '../../../assets/icon-nav-admin-acadêmico.svg'
+import IconAcadêmicoSeleSVG from '../../../assets/icon-nav-admin-acadêmico-sele.svg'
+import IconDashBoardSVG from '../../../assets/icon-nav-admin-dashBoard.svg'
+import IconDashBoardSeleSVG from '../../../assets/icon-nav-admin-dashBoard-sele.svg'
+import IconMarketingSVG from '../../../assets/icon-nav-admin-marketing.svg'
+import IconMarketingSeleSVG from '../../../assets/icon-nav-admin-marketing-sele.svg'
+import IconFinanceiroSVG from '../../../assets/icon-nav-admin-financeiro.svg'
+import IconFinanceiroSeleSVG from '../../../assets/icon-nav-admin-financeiro-sele.svg'
+import IconColaboradoresSVG from '../../../assets/icon-nav-admin-financeiro.svg'
+import IconColaboradoresSeleSVG from '../../../assets/icon-nav-admin-financeiro-sele.svg'
 
 const Container = styled.div`
     display: grid;
-    grid-template-columns: 0.4fr 2.6fr;
+    grid-template-columns: 0.5fr 2.5fr;
 `
 
 const Main = styled.main`
@@ -29,23 +40,81 @@ const LogoJPNome = styled(LogoJPNomeSemStyle)`
 
 const Funções = styled.ul`
     height: auto;
-    font-size: 1vw;
+    font-size: 1.5vw;
     font-weight: 500;
-    margin-top: 5%;
+    margin-top: 15%;
 `
 
 const Função = styled.li`
     height: auto;
     color: ${props => props.selected ? '#000000;' :'#9D9D9D;'};
     width: 80%;
-    display: block;
+    display: flex;
     margin-left: auto;
     margin-right: auto;
     padding: 3%;
+    margin-top: 4%;
+    align-items: center;
 `
 
 const IconAlunos = styled(IconAlunosSVG)`
-    width: 20%;
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconAlunosSele = styled(IconAlunosSeleSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconAcadêmico = styled(IconAcadêmicoSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconAcadêmicoSele = styled(IconAcadêmicoSeleSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconDashBoard = styled(IconDashBoardSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconDashBoardSele = styled(IconDashBoardSeleSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconMarketing = styled(IconMarketingSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconMarketingSele = styled(IconMarketingSeleSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconFinanceiro = styled(IconFinanceiroSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconFinanceiroSele = styled(IconFinanceiroSeleSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconColaboradores = styled(IconColaboradoresSVG)`
+    width: 35%;
+    margin-right: 8%;
+`
+
+const IconColaboradoresSele = styled(IconColaboradoresSeleSVG)`
+    width: 35%;
+    margin-right: 8%;
 `
 
 export { 
@@ -55,5 +124,16 @@ export {
     Funções,
     Função,
     IconAlunos,
+    IconAlunosSele,
+    IconAcadêmico,
+    IconAcadêmicoSele,
+    IconDashBoard,
+    IconDashBoardSele,
+    IconMarketing,
+    IconMarketingSele,
+    IconFinanceiro,
+    IconFinanceiroSele,
+    IconColaboradores,
+    IconColaboradoresSele,
     Main
 }
