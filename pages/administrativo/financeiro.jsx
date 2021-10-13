06bf974e78913c7c6b597aad94c0a7efdcbdd0c5
Miguelpenha <65141147+Miguelpenha@users.noreply.github.com>
2021-10-12 23:09:27 -0300

    Ajeitando problema no gráfico

--- pages/administrativo/financeiro.jsx
+++ pages/administrativo/financeiro.jsx
@@ -8,7 +8,7 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { get } from '../../hooks'
 import api from '../../services/api/base'
-import { PieChart, Pie, Sector, Cell } from 'recharts'
+import { PieChart, Pie, Cell } from 'recharts'
 
 export default function Financeiro() {
   const { data: totalReceitas, mutate: mutateTotalReceitas } = get('/financeiro/receitas/total')
@@ -517,25 +517,28 @@ export default function Financeiro() {
   }
   
   const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
-  ];
+    {
+      nome: 'Receitas',
+      valor: totalReceitas ? totalReceitas.totalBruto : null,
+      real: totalReceitas ? totalReceitas.total : null,
+      cor: '#60BF92'
+    },
+    {
+      nome: 'Despesas',
+      valor: totalDespesas ? totalDespesas.totalBruto : null,
+      real: totalDespesas ? totalDespesas.total : null,
+      cor: '#EF5252'
+    }
+  ]
   
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-  
-  const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({
-    cx, cy, midAngle, innerRadius, outerRadius, percent, index,
-  }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-  
+  const RADIAN = Math.PI / 180
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+    const x = cx + radius * Math.cos(-midAngle * RADIAN)
+    const y = cy + radius * Math.sin(-midAngle * RADIAN)
     return (
       <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`${(percent * 100).toFixed(0)}%`}
+        {data[index].real}
       </text>
     );
   };
@@ -618,18 +621,19 @@ export default function Financeiro() {
           </Infos>
           <PieChart width={400} height={400}>
             <Pie
+              isAnimationActive={false}
               data={data}
-              cx={200}
-              cy={200}
+              cx="50%"
+              cy="50%"
               labelLine={false}
               label={renderCustomizedLabel}
               outerRadius={80}
               fill="#8884d8"
-              dataKey="value"
+              dataKey="valor"
             >
-					    {data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
-				    </Pie>
-			    </PieChart>
+            {data.map((dado, index) => <Cell key={index} fill={dado.cor} />)}
+            </Pie>
+          </PieChart>
           <Menu anchorEl={fechadoCadas} open={openCadas} onClose={clickCloseCadas} MenuListProps={{
           'aria-labelledby': 'basic-button',
         }} style={{height: '62%', width: '32%'}}>
