08d33469cce4c31008658f85aca77b0995a45f94
Miguelpenha <65141147+Miguelpenha@users.noreply.github.com>
2021-10-17 20:29:42 -0300

    Deixando estilos da planilha obrigatorios

--- components/TableReceitasDespesas/style.js
+++ components/TableReceitasDespesas/style.js
@@ -2,27 +2,35 @@ import styled from 'styled-components'
 import { TableCell as TableCellMUI, TableContainer as TableContainerMUi, TableRow as TableRowMUI } from '@material-ui/core'
 
 export const TableContainer = styled(TableContainerMUi)`
-    width: 85%;
-    border-radius: 20px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 5%;
+    && {
+        width: 85%;
+        border-radius: 20px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 5%;
+    }
 `
 
 export const TableCell = styled(TableCellMUI)`
-    font-size: 1vw;
+    && {
+        font-size: 1vw;
+    }
 `
 
 export const TableCellBorder = styled(TableCell)`
-    border-right: 1px solid #E0E0E0;
+    && {
+        border-right: 1px solid #E0E0E0;
+    }
 `
 
 export const TableCellTitle = styled(TableCell)`
-    color: #0872FC;
-    font-size: 2vw;
-    padding-top: 5%;
-    padding-bottom: 5%;
+    && {
+        color: #0872FC;
+        font-size: 2vw;
+        padding-top: 5%;
+        padding-bottom: 5%;
+    }
 `
 
 export const TableRowSele = styled(TableRowMUI)`
@@ -34,31 +42,41 @@ export const TableRowSele = styled(TableRowMUI)`
 `
 
 export const TableCellValue = styled(TableCell)`
-    color: ${props => {
-        if (props.receita) {
-            return '#5AB55E'
-        } else if (props.noColor) {
-            return '#000000'
-        } else {
-            return '#ED3237'
-        }
-    }};
+    && {
+        color: ${props => {
+            if (props.receita) {
+                return '#5AB55E'
+            } else if (props.noColor) {
+                return '#000000'
+            } else {
+                return '#ED3237'
+            }
+        }};
+    }
 `
 
 export const TableCellValueBorder = styled(TableCellValue)`
-    border-left: 1px solid #E0E0E0;
+    && {
+        border-left: 1px solid #E0E0E0;
+    }
 `
 
 export const TableCellSaldo = styled(TableCell)`
-    font-size: 2vw;
-    padding: 5%;
+    && {
+        font-size: 2vw;
+        padding: 5%;
+    }
 `
 
 export const TextSaldo = styled.span`
-    color: ${props => props.negative ? '#ED3237' : '#5AB55E'};
-    margin-right: 2%;
+    && {
+        color: ${props => props.negative ? '#ED3237' : '#5AB55E'};
+        margin-right: 2%;
+    }
 `
 
 export const TextSaldoValue = styled.span`
-    color: ${props => props.negative ? '#ED3237' : '#5AB55E'};
+    && {
+        color: ${props => props.negative ? '#ED3237' : '#5AB55E'};
+    }
 `
