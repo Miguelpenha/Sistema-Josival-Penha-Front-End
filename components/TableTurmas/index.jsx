075bcff3f73f4988ea584556c377a8fb1ec0269d
Miguel Penha <miguel.penha1@gmail.com>
2021-10-26 23:45:03 -0300

    Criando tabela de turma da página de alunos

---
+++ components/TableTurmas/index.jsx
@@ -0,0 +1,100 @@
+import { TableContainer, TableCellTitle, TableCellTotal, TextTotal, TableRowSele, TableCellValueBorder, TableCellBorder, IconButtonExclu, TableCellTitleBorder, LimitText } from './style'
+import { Paper, Table, TableHead, TableRow, TableBody, TableFooter, Tooltip, Checkbox } from '@material-ui/core'
+import { Delete as DeleteIcon } from '@material-ui/icons'
+import { useState } from 'react'
+
+export default function TableTurmas({ turmas=[], onDeleteTurmas, onDeleteTurmasTodos }) {
+    if (typeof turmas != 'string' && turmas) {
+        const [selecionados, setSelecionados] = useState([])
+        turmas.map(turma => turma.criacao.sistema = new Date(turma.criacao.sistema))
+        
+        let rows = turmas
+        
+        function sortDate(a, b) {
+            return b.criacao.sistema - a.criacao.sistema
+        }
+        
+        rows.sort(sortDate)
+
+        function clickSele(id) {
+            let index = selecionados.indexOf(id)
+            if (index === -1) {
+                selecionados.push(id)
+                setSelecionados(selecionados)
+            } else {
+                let newSelecionados = selecionados.map(selecionado => selecionado != id && selecionado)
+            }
+        }
+
+        function Row({row, index}) {
+            return <TableRowSele key={index} onClick={() => clickSele(row._id)}>
+                        <TableCellValueBorder component="th" scope="col" align="center">
+                            <Checkbox sx={{'& .MuiSvgIcon-root': {fontSize: 20}}}/>
+                        </TableCellValueBorder>
+                        <TableCellValueBorder component="th" scope="col" align="center">
+                            <LimitText limit={16}>{row.nome}</LimitText>
+                        </TableCellValueBorder>
+                        <TableCellValueBorder component="th" scope="col" align="center">{row.alunos}</TableCellValueBorder>
+                        <TableCellValueBorder component="th" scope="col" align="center">{row.professora}</TableCellValueBorder>
+                        <TableCellValueBorder component="th" scope="col" align="center">{row.serie}</TableCellValueBorder>
+                        <TableCellValueBorder component="th" scope="col" align="center">{row.turno}</TableCellValueBorder>
+                        <TableCellValueBorder align="center">
+                            <Tooltip title={
+                                <span style={{fontSize: '1vw'}}>Excluir essa turma</span>
+                            } arrow placement="bottom">
+                                <IconButtonExclu onClick={() => onDeleteTurmas(row._id)}>
+                                    <DeleteIcon sx={{color: '#ED3237'}}/>
+                                </IconButtonExclu>
+                            </Tooltip>       
+                        </TableCellValueBorder>
+                </TableRowSele>
+        
+        }
+        
+        return (
+            <TableContainer component={Paper}>
+                <Table size="medium">
+                    <TableHead>
+                        <TableRow>
+                            <TableCellTitle align="center" scope="col" colSpan={6}>Turmas</TableCellTitle>
+                            <TableCellTitleBorder align="center" scope="col" colSpan={1}>
+                                <Tooltip title={
+                                    <span style={{fontSize: '1vw'}}>Excluir turmas</span>
+                                } arrow placement="bottom">
+                                    <IconButtonExclu onClick={() => onDeleteTurmasTodos()}>
+                                        <DeleteIcon sx={{color: '#ED3237'}}/>
+                                    </IconButtonExclu>
+                                </Tooltip>
+                            </TableCellTitleBorder>
+                        </TableRow>
+                        <TableRow>
+                            <TableCellBorder>
+                                <Checkbox sx={{'& .MuiSvgIcon-root': {fontSize: 24}}}/>
+                            </TableCellBorder>
+                            <TableCellBorder align="center">Nome</TableCellBorder>
+                            <TableCellBorder align="center">Alunos</TableCellBorder>
+                            <TableCellBorder align="center">Professora</TableCellBorder>
+                            <TableCellBorder align="center">Série</TableCellBorder>
+                            <TableCellBorder align="center">Turno</TableCellBorder>
+                            <TableCellBorder align="center">Opções</TableCellBorder>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row, index) => (
+                            <Row row={row} index={index}/>
+                        ))}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCellTotal align="center" colSpan={7}>
+                                <TextTotal>{rows.length}{rows.length === 1 ? ' turma' : ' turmas'}</TextTotal>
+                            </TableCellTotal>
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </TableContainer>
+        )
+    } else {
+        return null
+    }
+}
---
+++ components/TableTurmas/style.js
@@ -0,0 +1,138 @@
+import styled, { css } from 'styled-components'
+import { TableCell as TableCellMUI, TableContainer as TableContainerMUi, TableRow as TableRowMUI, IconButton as IconButtonBruto, Checkbox } from '@material-ui/core'
+import Image from 'next/image'
+
+export const TableContainer = styled(TableContainerMUi)`
+    && {
+        width: 95%;
+        border-radius: 20px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 5%;
+        margin-bottom: 5%;
+    }
+`
+
+export const TableCell = styled(TableCellMUI)`
+    && {
+        font-size: 1vw;
+    }
+`
+
+export const TableCellBorder = styled(TableCell)`
+    && {
+        border-right: 1px solid #E0E0E0;
+    }
+`
+
+export const TableCellTitle = styled(TableCell)`
+    && {
+        color: #0872FC;
+        font-size: 2vw;
+        padding-top: 3%;
+        padding-bottom: 3%;
+    }
+`
+
+export const TableCellTitleBorder = styled(TableCell)`
+    && {
+        color: #0872FC;
+        font-size: 2vw;
+        padding-top: 3%;
+        padding-bottom: 3%;
+        border-left: 1px solid #E0E0E0;
+    }
+`
+
+export const TableRowSele = styled(TableRowMUI)`
+    &:hover {
+        transition-timing-function: linear;
+        transition-duration: 0.2s;
+    }
+`
+
+export const TableCellValue = styled(TableCell)`
+        && {
+            color: #000000
+            ${props => props.bold && css`
+                font-weight: bold;
+            `}
+        }
+    `
+
+export const TableCellValueBorder = styled(TableCellValue)`
+    && {
+        border-left: 1px solid #E0E0E0;
+    }
+`
+
+export const TableCellTotal = styled(TableCell)`
+    && {
+        font-size: 2vw;
+        padding: 5%;
+    }
+`
+
+export const TextTotal = styled.span`
+    && {
+        color: #0872FC;
+        margin-right: 2%;
+    }
+`
+
+export const TextSaldoValue = styled.span`
+    && {
+        color: #000000;
+    }
+`
+
+export const IconButtonExclu = styled(IconButtonBruto)`
+    && {
+        background-color: #FBD6D7;
+    }
+`
+
+export const IconButtonMais = styled(IconButtonBruto)`
+    && {
+        background-color: #bfbfbfd3;
+    }
+`
+
+export const CheckBox = styled(Checkbox)`
+    & svg {
+        font-size: 1.5vw;
+    }
+`
+
+export const LinkFotoAluno = styled.a`
+    &:hover {
+        img {
+            border-radius: 20%;
+        }
+    }
+`
+
+export const FotoAluno = styled.img`
+    border-radius: 50%;
+
+    &:hover {
+        border-radius: 20%;
+    }
+`
+
+export function LimitText({ children='', limit=0, points=true }) {
+    if (children.length > limit) {
+        let newChildren = ''
+        
+        for (let index = 1;index <= limit;index++) newChildren += children[index-1]
+        
+        if (points) {
+            return newChildren+'...'
+        } else {
+            return newChildren
+        }
+    } else {
+        return children
+    }
+}
