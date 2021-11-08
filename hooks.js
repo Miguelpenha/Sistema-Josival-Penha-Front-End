6876e9065efe0de35224d16c9f1f767dc1e3c60f
Miguel Penha <miguel.penha1@gmail.com>
2021-11-08 00:15:52 -0300

    Adicionando key da api

--- hooks.js
+++ hooks.js
@@ -2,7 +2,10 @@ import useSWR from 'swr'
 import axios from 'axios'
 
 const base = axios.create({
-    baseURL: process.env.NEXT_STATIC_API_URL
+    baseURL: process.env.NEXT_STATIC_API_URL,
+    headers: {
+        'Authorization': `key ${process.env.NEXT_STATIC_API_KEY}`
+    }
 })
 
 export default function api(url='', config={}) {
--- services/api/base.js
+++ services/api/base.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const base = axios.create({
-    baseURL: process.env.NEXT_STATIC_API_URL
+    baseURL: process.env.NEXT_STATIC_API_URL,
+    headers: {
+        'Authorization': `key ${process.env.NEXT_STATIC_API_KEY}`
+    }
 })
 
 export default base
