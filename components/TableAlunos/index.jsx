07ebd4cf4b138392367880d08207804d40e9b8f5
Miguel Penha <miguel.penha1@gmail.com>
2021-11-22 20:50:28 -0300

    Ajeitando form de baixar planilha

--- components/TableAlunos/index.jsx
+++ components/TableAlunos/index.jsx
@@ -6,7 +6,6 @@ import { useState, memo } from 'react'
 
 function TableAlunos({ alunos=[], onDeleteAlunos, onDeleteAlunosTodos, bg }) {
     if (typeof alunos != 'string' && alunos) {
-        const [openModelPlanilha, setOpenModelPlanilha] = useState(false)
         alunos.map(aluno => aluno.criação.sistema = new Date(aluno.criação.sistema))
         
         let rows = alunos
@@ -20,25 +19,6 @@ function TableAlunos({ alunos=[], onDeleteAlunos, onDeleteAlunosTodos, bg }) {
         function calcIdade(nascimento='', hoje) {
             nascimento = new Date(`${nascimento.split('/')[2]}-${nascimento.split('/')[1]}-${nascimento.split('/')[0]}`)
             return Math.floor(Math.ceil(Math.abs(nascimento.getTime()-hoje.getTime())/(1000*3600*24))/365.25)
-        }
-
-        function ModelPlanilha({ open }) {
-            if (open) {
-                return (
-                    <DialogGerarDeclaração open={true} onClose={() => setOpenModelPlanilha(false)}>
-                        <DialogContent>
-                            <form method="POST" action={`${process.env.NEXT_STATIC_API_URL}/alunos/exportar`}>
-                                <input type="hidden" name="keyapi" value={process.env.NEXT_STATIC_API_KEY}/>
-                                <BolsistaSwitch defaultChecked name="protegido"/>Protegido
-                                <InputPorcentagemGerarDeclaração name="senha" placeholder="Senha para proteção da planilha" type="password" variant="standard" style={{width: '100%'}}/>
-                                <ButtonSubmitGerarDeclaração style={{marginBottom: '0%'}} type="submit" variant="contained">Gerar</ButtonSubmitGerarDeclaração>
-                            </form>
-                        </DialogContent>
-                    </DialogGerarDeclaração>
-                )
-            }
-
-            return null
         }
 
         function Row({row, index}) {
@@ -205,11 +185,13 @@ function TableAlunos({ alunos=[], onDeleteAlunos, onDeleteAlunosTodos, bg }) {
                                 <Tooltip title={
                                     <span style={{fontSize: '1vw'}}>Baixar planilha</span>
                                 } arrow placement="bottom">
-                                    <IconButtonExclu type="submit" style={{left: '5%'}} bg="#B5D5FE" onClick={() => setOpenModelPlanilha(true)}>
-                                        <DownloadIcon sx={{color: '#0872FC'}}/>
-                                    </IconButtonExclu>
+                                    <form method="POST" action={`${process.env.NEXT_STATIC_API_URL}/alunos/exportar`} style={{display: 'inline-block'}}>
+                                        <input type="hidden" name="keyapi" value={process.env.NEXT_STATIC_API_KEY}/>
+                                        <IconButtonExclu type="submit" style={{left: '5%'}} bg="#B5D5FE">
+                                            <DownloadIcon sx={{color: '#0872FC'}}/>
+                                        </IconButtonExclu>
+                                    </form>
                                 </Tooltip>
-                                <ModelPlanilha open={openModelPlanilha}/>
                             </TableCellTitleBorder>
                         </TableRow>
                         <TableRow>
