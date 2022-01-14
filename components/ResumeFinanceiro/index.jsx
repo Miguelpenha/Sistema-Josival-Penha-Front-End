9d9bf4cb27301c47c2031d9a983a73b8f8d57402
Miguel Penha <miguel.penha1@gmail.com>
2022-01-14 08:26:57 -0300

    Update index.jsx

--- components/ResumeFinanceiro/index.jsx
+++ components/ResumeFinanceiro/index.jsx
@@ -2,23 +2,27 @@ import { Container } from './style'
 import ReceitaOrDespesa from './ReceitaOrDespesa'
 
 export default function ResumeFinanceiro({ receitas, despesas }) {
-    receitas.map(receita => {
-        receita.receita = true
-        receita.criação.sistema = new Date(receita.criação.sistema)
-    })
-
-    despesas.map(despesa => {
-        despesa.despesa = true
-        despesa.criação.sistema = new Date(despesa.criação.sistema)
-    })
-
-    const receitasDespesas = [...receitas, ...despesas]
-
-    return (
-        <Container>
-            {receitasDespesas.map((receitaDespesa, index) => (
-                <ReceitaOrDespesa name={receitaDespesa.nome} value={receitaDespesa.preco} receita={receitaDespesa.receita ? true : false}/>
-            ))}
-        </Container>
-    )
+    if (typeof receitas != 'string' && typeof despesas != 'string') {
+        receitas.map(receita => {
+            receita.receita = true
+            receita.criação.sistema = new Date(receita.criação.sistema)
+        })
+    
+        despesas.map(despesa => {
+            despesa.despesa = true
+            despesa.criação.sistema = new Date(despesa.criação.sistema)
+        })
+    
+        const receitasDespesas = [...receitas, ...despesas]
+    
+        return (
+            <Container>
+                {receitasDespesas.map((receitaDespesa, index) => (
+                    <ReceitaOrDespesa name={receitaDespesa.nome} value={receitaDespesa.preco} receita={receitaDespesa.receita ? true : false}/>
+                ))}
+            </Container>
+        )
+    } else {
+        return null
+    }
 }
