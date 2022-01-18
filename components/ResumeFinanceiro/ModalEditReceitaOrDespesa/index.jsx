3c7dcb8f15a65d2ce08d91643a36378d1ef3dbca
Miguel Penha <miguel.penha1@gmail.com>
2022-01-18 10:26:42 -0300

    Começando a fazer modal de editar receita-despesa

--- components/ResumeFinanceiro/ModalEditReceitaOrDespesa/index.jsx
+++ components/ResumeFinanceiro/ModalEditReceitaOrDespesa/index.jsx
@@ -1,5 +1,5 @@
 import { Modal } from '@material-ui/core'
-import { Container } from './style'
+import { Container, Form, Campo, Label, InputText } from './style'
 
 export default function ModalEditReceitaOrDespesa({ open, onClose, receitaOrDespesa }) {
     return (
@@ -8,7 +8,27 @@ export default function ModalEditReceitaOrDespesa({ open, onClose, receitaOrDesp
             onClose={onClose}
         >
             <Container receita={receitaOrDespesa.receita}>
-                <h1>{receitaOrDespesa.nome}</h1>
+                <h1>Editar {receitaOrDespesa.receita ? 'Receita' : 'Despesa'}</h1>
+                <Form>
+                    <Campo>
+                        <Label htmlFor="Nome">Nome</Label>
+                        <InputText
+                            type="text"
+                            placeholder="Nome: "
+                            receita={receitaOrDespesa.receita}
+                            defaultValue={receitaOrDespesa.nome}
+                        />
+                    </Campo>
+                    <Campo>
+                        <Label htmlFor="Valor">Valor</Label>
+                        <InputText
+                            type="text"
+                            placeholder="Valor: "
+                            receita={receitaOrDespesa.receita}
+                            defaultValue={receitaOrDespesa.preco}
+                        />
+                    </Campo>
+                </Form>
             </Container>
         </Modal>
     )
--- components/ResumeFinanceiro/ModalEditReceitaOrDespesa/style.js
+++ components/ResumeFinanceiro/ModalEditReceitaOrDespesa/style.js
@@ -15,3 +15,38 @@ export const Container = styled.div`
     height: 50%;
     align-items: center;
 `
+
+export const Form = styled.form`
+    display: flex;
+    width: 70%;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 2%;
+`
+
+export const Campo = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    padding-bottom: 4%;
+`
+
+export const Label = styled.label`
+    font-size: 1.5vw;
+    padding-bottom: 2%;
+`
+
+export const InputText = styled.input`
+    width: 70%;
+    border: 2px solid ${props => props.receita ? '#60BF92' : '#EF5252'};
+    color: ${props => props.receita ? '#60BF92' : '#EF5252'};
+    border-radius: 10px;
+    font-size: 1.5vw;
+    padding: 1.5%;
+
+    :focus {
+        outline: none;
+        border-width: 3px;
+        border-radius: 15px;
+    }
+`
