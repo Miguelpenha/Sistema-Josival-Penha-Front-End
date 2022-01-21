a9f6b85f76535014c5bb8d8e4fa45cf9c72bfd26
Miguel Penha <miguel.penha1@gmail.com>
2022-01-20 21:59:30 -0300

    Update global.js

--- styles/global.js
+++ styles/global.js
@@ -10,7 +10,7 @@ export default createGlobalStyle`
     }
 
     html, body, body>div:first-child, div#__next, div#__next>div {
-        height: 98.5%;
+        height: 100%;
     }
     
     html {
