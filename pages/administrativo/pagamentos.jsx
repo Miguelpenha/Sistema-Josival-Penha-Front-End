5997e73553d7985028edad1d53069c767425084d
Miguel Penha <miguel.penha1@gmail.com>
2022-01-27 04:57:52 -0300

    Update pagamentos.jsx

--- pages/administrativo/pagamentos.jsx
+++ pages/administrativo/pagamentos.jsx
@@ -3,11 +3,17 @@ import { useState } from 'react'
 import Head from 'next/head'
 import { Container, Main } from '../../styles/pages/administrativo/pagamentos'
 import NavOptions from '../../components/pages/administrativo/pagamentos/NavOptions'
+import { Select, MenuItem, Switch } from '@material-ui/core'
 import ModalMensalidade from '../../components/pages/administrativo/pagamentos/ModalMensalidade'
 import nookies from 'nookies'
 
 export default function Pagamentos() {
+  const { data: turmas } = get('/turmas')
   const { data: alunos } = get('/alunos')
+  const [mesFilter, setMesFilter] = useState(new Date().toLocaleDateString('pt-br').split('/')[1])
+  const [textFilter, setTextFilter] = useState('')
+  const [turmaFilter, setTurmaFilter] = useState('Nenhuma turma')
+  const [atrazadosFilter, setAtrazadosFilter] = useState(false)
   const [openModalMensalidade, setOpenModalMensalidade] = useState(false)
   const [aluno, setAluno] = useState(null)
   const [mesMensalidade, setMesMensalidade] = useState(null)
@@ -26,11 +32,67 @@ export default function Pagamentos() {
                 onClick={() => {
                   handleOpenModalMensalidade()
                   setAluno(alunos[0])
-                  setMesMensalidade('01')
+                  setMesMensalidade('02')
                 }}
               >
                 Pagamentos
               </h1>
+              <input type="text" value={textFilter} onChange={event => setTextFilter(event.target.value)}/>
+              <Select value={mesFilter} onChange={event => setMesFilter(event.target.value)}>
+                <MenuItem value="01">Janeiro</MenuItem>
+                <MenuItem value="02">Fevereiro</MenuItem>
+                <MenuItem value="03">Março</MenuItem>
+                <MenuItem value="04">Abril</MenuItem>
+                <MenuItem value="05">Maio</MenuItem>
+                <MenuItem value="06">Junho</MenuItem>
+                <MenuItem value="07">Julho</MenuItem>
+                <MenuItem value="08">Agosto</MenuItem>
+                <MenuItem value="09">Setembro</MenuItem>
+                <MenuItem value="10">Outubro</MenuItem>
+                <MenuItem value="11">Novembro</MenuItem>
+                <MenuItem value="12">Dezembro</MenuItem>
+              </Select>
+              <Select value={turmaFilter} onChange={event => setTurmaFilter(event.target.value)}>
+                <MenuItem value="Nenhuma turma">Nenhuma turma</MenuItem>
+                {turmas && turmas.map((turma, index) => (
+                  <MenuItem value={turma._id} key={index}>{turma.nome}</MenuItem>
+                ))}
+              </Select>
+              <br/>
+              <Switch checked={atrazadosFilter} onChange={event => setAtrazadosFilter(event.target.checked)}/>Atrazados
+              {turmas && turmas.map((turma, index) => {
+                if (turmaFilter === 'Nenhuma turma' || turma._id === turmaFilter) {
+                  return (
+                    <table key={index}>
+                      <thead>
+                        <tr>
+                          <th>Aluno</th>
+                          <th>Status</th>
+                          <th>Valor</th>
+                          <th>Forma de pagamento</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {alunos && alunos.map((aluno, index) => {
+                          if(aluno.turma === turma.nome && aluno.nome.toUpperCase().includes(textFilter.toUpperCase())) {
+                            const veriAtrazado = aluno.pagamentos[mesFilter].pago ? false : new Date().getMonth()+1 == mesFilter ? Number(new Date(new Date().getFullYear(), mesFilter-1, Number(new Date().toLocaleDateString('pt-br').split('/')[0])).toLocaleDateString('pt-br').split('/')[0]) <= Number(aluno.pagamentos[mesFilter].vencimento.split('/')[0]) ? false : true : Number(new Date(new Date().getFullYear(), mesFilter-1, 1).toLocaleDateString('pt-br').split('/')[0]) <= Number(aluno.pagamentos[mesFilter].vencimento.split('/')[0]) ? false : true
+                            if (atrazadosFilter ? veriAtrazado : true) {
+                              return (
+                                <tr key={index}>
+                                  <td>{aluno.nome}</td>
+                                  <td>{aluno.pagamentos[mesFilter].pago ? 'Em dia' : new Date().getMonth()+1 == mesFilter ? Number(new Date(new Date().getFullYear(), mesFilter-1, Number(new Date().toLocaleDateString('pt-br').split('/')[0])).toLocaleDateString('pt-br').split('/')[0]) <= Number(aluno.pagamentos[mesFilter].vencimento.split('/')[0]) ? 'Em dia' : 'Atrazado' : Number(new Date(new Date().getFullYear(), mesFilter-1, 1).toLocaleDateString('pt-br').split('/')[0]) <= Number(aluno.pagamentos[mesFilter].vencimento.split('/')[0]) ? 'Em dia' : 'Atrazado'}</td>
+                                  <td>{aluno.pagamentos[mesFilter].value}</td>
+                                  <td>{aluno.pagamentos[mesFilter].forma}</td>
+                                </tr>
+                              )
+                            }
+                          }
+                        })}
+                      </tbody>
+                    </table>
+                  )
+                }
+              })}
               <ModalMensalidade
                 open={openModalMensalidade}
                 onClose={handleCloseModalMensalidade}
