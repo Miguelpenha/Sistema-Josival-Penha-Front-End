d8a6154a11b3902a4cd564bea346aa2026fbdbe4
Miguel Penha <miguel.penha1@gmail.com>
2022-01-29 03:52:38 -0300

    Começando página de pagamentos por aluno

--- pages/administrativo/pagamentos/[aluno].jsx
+++ pages/administrativo/pagamentos/[aluno].jsx
@@ -1,7 +1,19 @@
-import { useRouter } from "next/router"
+import { useRouter } from 'next/router'
+import { get } from '../../../hooks'
+import { Container } from '../../../styles/pages/administrativo/pagamentos/aluno'
 
 export default function PagamentosAluno({  }) {
-    const { aluno } = useRouter().query
+    const { aluno: alunoId } = useRouter().query
 
-    return <h1>{aluno && aluno}</h1>
+    if (alunoId) {
+        const { data: aluno } = get(`/alunos/${alunoId}`)
+        
+        return (
+            <Container>
+                <h1>{aluno && aluno.nome}</h1>
+            </Container>
+        )
+    } else {
+        return null
+    }
 }
