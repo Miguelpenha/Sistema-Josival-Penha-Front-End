0e08fc2b44c305f4358f5c93388a3175f536f9a2
Miguel Penha <miguel.penha1@gmail.com>
2022-02-06 11:20:16 -0300

    Ajeitando página principal desktop

--- pages/desktop/administrativo.jsx
+++ pages/desktop/administrativo.jsx
@@ -1,30 +1,40 @@
-import nookies from 'nookies'
-import { Container, Title, ButtonFile, IconFile, AnimationControl, IconBack, IconButtonBack } from '../../styles/pages/desktop/administrativo'
+import { Container, ContainerIconBack, IconBack, Title, ContainerIconFile, IconFile, Animation } from '../../styles/pages/desktop/administrativo'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+import nookies from 'nookies'
 
 export default function Administrativo() {
+  const router = useRouter()
+
   return (
     <Container>
-      <Link href="/">
-        <IconButtonBack color="primary" component="a" onClick={() => window.desktop.logout()}>
-            <IconBack fontSize="large"/>
-        </IconButtonBack>
-      </Link>
+      <ContainerIconBack title="Sair do sistema" onClick={() => {
+        window.desktop.logout()
+        router.push('/')
+      }}>
+        <IconBack xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M0 0h24v24H0z" fill="none"/>
+          <path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
+        </IconBack>
+      </ContainerIconBack>
       <Title>Painel de controle Josival Penha</Title>
-      <Link href="arquivos">
-        <ButtonFile variant="contained" component="a">
+      <Link href="/desktop/arquivos">
+        <ContainerIconFile title="Ver arquivos">
           Ver arquivos
-          <IconFile fontSize="large"/>
-        </ButtonFile>
+          <IconFile xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M6 2c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6H6zm7 7V3.5L18.5 9H13z"/>
+          </IconFile>
+        </ContainerIconFile>
       </Link>
-      <AnimationControl options={{
+      <Animation options={{
         animationData: require('../../animations/control.json'),
         autoplay: true,
         loop: true,
         rendererSettings: {
             preserveAspectRatio: 'xMidYMid slice'
         }
-      }}/>
+      }} isClickToPauseDisabled/>
     </Container>
   )
 }
--- styles/pages/desktop/administrativo.js
+++ styles/pages/desktop/administrativo.js
@@ -1,6 +1,4 @@
 import styled from 'styled-components'
-import { Button, IconButton } from '@material-ui/core'
-import { InsertDriveFile, ExitToApp } from '@material-ui/icons'
 import Lottie from 'react-lottie'
 
 export const Container = styled.div`
@@ -8,38 +6,62 @@ export const Container = styled.div`
     flex-direction: column;
 `
 
-export const Title = styled.h1`
-    text-align: center;
-    font-size: 3vw;
-    margin-top: 4%;
-    margin-bottom: 2%;
+export const ContainerIconBack = styled.button`
+    width: 5%;
+    margin: 0.6%;
+    border: none;
+    padding: 0.5%;
+    display: flex;
     color: #009BDD;
-`
+    cursor: pointer;
+    border-radius: 50%;
+    align-items: center;
+    justify-content: center;
 
-export const ButtonFile = styled(Button)`
-    && {
-        background-color: #009BDD;
-        font-size: 1.5vw;
-        width: 17%;
-        align-self: center;
-        margin-top: 5%;
+    :hover {
+        background-color: #cccccc;
     }
 `
 
-export const IconFile = styled(InsertDriveFile)`
-    margin-left: 4%;
+export const IconBack = styled.svg`
+    width: 100%;
+    height: auto;
 `
 
-export const AnimationControl = styled(Lottie).attrs({
-    width: '50%'
+export const Title = styled.h1`
+    color: #009BDD;
+    font-size: 3vw;
+    margin-bottom: 4%;
+    text-align: center;
+`
+
+export const ContainerIconFile = styled.a`
+    width: 18%;
+    margin: auto;
+    padding: 1.5%;
+    display: flex;
+    color: #ffffff;
+    cursor: pointer;
+    font-weight: 500;
+    font-size: 1.8vw;
+    border-radius: 10px;
+    align-items: center;
+    justify-content: center;
+    background-color: #009BDD;
+
+    :hover {
+        opacity: 0.7;
+    }
+`
+
+export const IconFile = styled.svg`
+    width: 14%;
+    margin-left: 8%;
+`
+
+export const Animation = styled(Lottie).attrs({
+    width: '58%',
+    height: 'auto'
 })`
-    
-`
-
-export const IconButtonBack = styled(IconButton)`
-    width: fit-content;
-`
-
-export const IconBack = styled(ExitToApp)`
     
 `
